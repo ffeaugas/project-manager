cac4f0d68b0b64aadbca8dc7f826c21a5750e739
ffeaugas <feaugas.francis@gmail.com>
2025-11-05 00:18:47 +0100

    fix: display fix

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className="flex flex-col w-full">
       <div className="flex flex-col-reverse md:flex-row h-dvh">
         <Kanban />
-        <div className="w-full md:w-[400px] md:h-dvh border-l border-zinc-700 flex flex-col gap-2 p-2 bg-zinc-900 md:block hidden">
+        <div className="w-full md:w-[400px] md:h-dvh border-l border-zinc-700 md:flex flex-col gap-2 p-2 bg-zinc-900 hidden">
           <CalendarSection />
           <ActivitySection />
         </div>
